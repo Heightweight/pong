b227e8b5889b5b4c73dee9c1911e8b255af22d3e
HW <narniskiylev@gmail.com>
2021-08-24 16:39:10 +0200

    Add tests + rework collision + beautify

--- test/LogicTest.hs
+++ test/LogicTest.hs
@@ -1,4 +1,5 @@
 import App.Logic
+import App.Draw.Render (leaderboardSort)
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
@@ -17,6 +18,9 @@ unitTests = testGroup "Unit tests"
   , testCase "paddle angle right" $ (pi/8) @=? (paddleAngle 230 280)
   , testCase "paddle angle left" $ (7*pi/8) @=? (paddleAngle 50 0)
   , testCase "paddle angle middle" $ (pi/2) @=? (paddleAngle 599 599)
+  , testCase "time to text" $ ("1:0:0") @=? (timeAsText $ startWorld {time = 60})
+  , testCase "score to text" $ ("0:0") @=? (scoreAsText $ startWorld)
+  , testCase "leaderboard sorting" $ ((head . leaderboardSort $ ["1:0:1 7:0", "1:0:0 7:1", "0:59:0 6:0"]) @=? "1:0:0 7:1")
   ]
 
 instance Arbitrary Game where
@@ -58,6 +62,9 @@ prop_normalizedID f = (normalize . normalize $ f) == (normalize f)
 smallFloat :: Gen Float
 smallFloat = choose (0, 1/60)
 
+averageFloat :: Gen Float
+averageFloat = choose (1/30, 1/60)
+
 spacedBallWorld :: Gen World
 spacedBallWorld = do
   ballX <- choose (200, 600) :: Gen Float
@@ -84,6 +91,57 @@ scoreWorld = do
   w <- arbitrary :: Gen World
   return $ w {game = (game w) {ball = (ballX, ballY)}}
 
+xCollisionWorld :: Gen World
+xCollisionWorld = do
+  ballX <- oneof [choose (0, 9), choose (791, 800)] :: Gen Float
+  ballY <- choose (200, 600) :: Gen Float
+  w <- arbitrary :: Gen World
+  return $ w {game = (game w) {ball = (ballX, ballY), dir = pi/2}}
+
+yCollisionWorld :: Gen World
+yCollisionWorld = do
+  ballY <- oneof [choose (0, 9), choose (791, 800)] :: Gen Float
+  ballX <- choose (200, 600) :: Gen Float
+  w <- arbitrary :: Gen World
+  return $ w {game = (game w) {ball = (ballX, ballY), dir = pi}}
+
+pPaddleCollisionWorld :: Gen World
+pPaddleCollisionWorld = do
+  w <- arbitrary
+  ballY <- choose (770, 800) :: Gen Float
+  let paddleX = (p1 . game $ w)
+  ballX <- choose (paddleX-50, paddleX + 50) :: Gen Float
+  let ballXN = max 0 $ min 800 ballX
+  return $ w {game = (game w) {ball = (ballXN, ballY), dir = pi/2}}
+
+aiPaddleCollisionWorld :: Gen World
+aiPaddleCollisionWorld = do
+  w <- arbitrary
+  ballY <- choose (0, 30) :: Gen Float
+  let paddleX = (p2 . game $ w)
+  ballX <- choose (paddleX-50, paddleX + 50) :: Gen Float
+  let ballXN = max 0 $ min 800 ballX
+  return $ w {game = (game w) {ball = (ballXN, ballY), dir = -pi/2}}
+
+prop_pPaddleCollision :: Float -> Property
+prop_pPaddleCollision f =
+  forAll pPaddleCollisionWorld $
+  \w -> ((dir . game $ w) /= (dir . game . (collisionPlayerPaddle f) $ w))
+
+prop_aiPaddleCollision :: Float -> Property
+prop_aiPaddleCollision f =
+  forAll aiPaddleCollisionWorld $
+  \w -> ((dir . game $ w) /= (dir . game . (collisionAIPaddle f) $ w))
+
+prop_xCollision :: Float -> Property
+prop_xCollision f =
+  forAll xCollisionWorld $
+  \w -> ((dir . game . (collisionX f) $ w) + (dir . game $ w) == pi)
+
+prop_yCollision :: Float -> Property
+prop_yCollision f =
+  forAll yCollisionWorld $
+  \w -> ((dir . game . (collisionY f) $ w) + (dir . game $ w) == 0)
 
 prop_spacedMovement :: Float -> Property
 prop_spacedMovement f =
@@ -110,10 +168,17 @@ prop_playerWin w = (Player ==) . result . gameOver $ w
 prop_playerLoss :: World -> Bool
 prop_playerLoss w = (AI ==) . result . gameOver $ w
 
+prop_popBall :: World -> Bool
+prop_popBall w = (ballX >= 11) && (ballX <= 789) && (ballY >= 11) && (ballY <= 789) where
+  ballX = fst . ball . game . popBall $ w
+  ballY = snd . ball . game . popBall $ w
+
+prop_tickTime :: Float -> World -> Bool
+prop_tickTime f w = ((time . (tickTime f) $ w) ==(time w) + f)
 
 quickTests = testGroup "QuickCheck tests"
   [
-  testProperty "normalization test" prop_normalized
+  testProperty "normalize test" prop_normalized
   , testProperty "normalization identity test" prop_normalizedID
   , testProperty "spaced ball movement" (forAll smallFloat prop_spacedMovement)
   , testProperty "scoring worlds" (forAll scoreWorld prop_scoreCheck)
@@ -121,4 +186,10 @@ quickTests = testGroup "QuickCheck tests"
   , testProperty "ai behaviour" (forAll spacedBallWorld prop_aiBehaviour)
   , testProperty "checking player's wins" (forAll wonGame prop_playerWin)
   , testProperty "checking player's losses" (forAll lostGame prop_playerLoss)
+  , testProperty "horizontal collision" (forAll averageFloat prop_xCollision)
+  , testProperty "vertical collision" (forAll averageFloat prop_yCollision)
+  , testProperty "player paddle collision" (forAll averageFloat prop_pPaddleCollision)
+  , testProperty "ai paddle collision" (forAll averageFloat prop_aiPaddleCollision)
+  , testProperty "checking collision resolvation" prop_popBall
+  , testProperty "time tick" (forAll smallFloat prop_tickTime)
   ]
